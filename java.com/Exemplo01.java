face788e859c14f2cc855546903a0be85e049be3
kauav244 <kaua.00000855612@unicap.br>
2025-02-18 23:48:46 +0000

    exemplo01

---
+++ java.com/Exemplo01.java
@@ -0,0 +1,14 @@
+public class Exemplo01{
+    public static void main(String[] args ){
+      System.out.println("Olá, turma!");
+      System.out.println("O dobro de 45.4 é " + dobro (45.4));
+    }
+
+    public static int dobro (int num){
+      return 2 * num;
+    }
+
+    public static double dobro(double num) {
+        return 2 * num;
+    }
+}
