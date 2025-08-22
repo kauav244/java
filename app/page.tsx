a459f1c5e6b565fa04511461b3997f10ad10e44e
kauã vitor <kaua.00000855612@unicap.br>
2025-08-22 00:22:33 +0000

    tudo

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Profile } from "@/componets/Profile";
+
+export default function Home() {
+  return (
+      <section>
+        <h1>Site criado para a disciplina Programção Front-End</h1>
+        <Profile />
+        <hr />
+      </section>
+  );
+}
---
+++ componets/Profile.tsx
@@ -0,0 +1,21 @@
+
+
+export function Profile() {
+
+    let img = "workspaces/java/app/image.jpeg"
+
+  return (
+    <div>
+      <img src="https://www.google.com/imgres?q=bob%20marley&imgurl=https%3A%2F%2Fwww.blogueirinho.com%2Fwp-content%2Fuploads%2F2023%2F12%2FA-historia-de-Bob-Marley-o-rei-do-reggae-que-levou-sua-musica-e-sua-mensagem-para-o-mundo.jpg&imgrefurl=https%3A%2F%2Fwww.blogueirinho.com%2Fa-historia-de-bob-marley%2F&docid=1KrkgkSOAt13eM&tbnid=kKklQAyXQWT5GM&vet=12ahUKEwjv5Kjpkp2PAxX7rJUCHcFFFm8QM3oECCMQAA..i&w=1280&h=720&hcb=2&ved=2ahUKEwjv5Kjpkp2PAxX7rJUCHcFFFm8QM3oECCMQAA" alt="foto de perfil" width={128} height={128} />
+      <h1>Kauã Vitor</h1>
+      <p>Eu adoro me perder em novos mundos, seja mergulhando em um livro interessante, assistindo a um filme que me
+         faça pensar, ou ouvindo uma música que me transporte para outro lugar. Também sou apaixonado por criar, seja desenhando,
+          programando ou até escrevendo umas ideias que vêm na cabeça. Gosto de me desafiar, de aprender coisas novas, mas também 
+          valorizo aqueles momentos mais calmos, de uma boa conversa ou um café no final do dia. No fundo, acho que o que me move mesmo é
+         a curiosidade e o prazer de criar e descobrir.
+
+        </p>
+        <img src="/workspaces/java/public/vercel.svg" alt="" width={200} height={200} />
+    </div>
+  );
+}
